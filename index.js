20ea40d80e60280ffca2ca5c119313d4e4c84e89
MikeWentForth <themikewallace@gmail.com>
2023-09-04 10:59:38 -0700

    Worked on async functionality and prompt questions

--- index.js
+++ index.js
@@ -21,15 +21,121 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 
-
 // Prep the inquirer questions menu
-let questions = [
+const questions = [
     {
         type: "list",
         name: "menuChoice",
         message: "What would you like to do?",
-        options: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee role"]
-    }
+        choices: [
+            "view all departments",
+            "view all roles",
+            "view all employees",
+            "add a department",
+            "add a role",
+            "add an employee",
+            "update an employee role",
+            "quit"
+        ]
+    },
+
 ];
+
+async function init() {
+
+    // Ask the questions....
+    let exit = false;
+    while (exit == false) {
+
+        // Ask the main question
+        console.log(); // blank line
+        let answers = await inquirer.prompt(questions);
+
+        // Respond based on the answer...
+        if (answers.menuChoice == "view all departments") {
+            console.log("Access DB, get list of all departments, display list.");
+            // Access DB
+            // Get a list of all depts
+            // Display the list
+        }
+
+        if (answers.menuChoice == "view all roles") {
+            console.log("Access DB, get list of all roles display list.");
+            // Access DB
+            // Get a list of all roles
+            // Display the list
+        }
+
+        if (answers.menuChoice == "view all employees") {
+            console.log("Access DB, get list of all employees, display list.");
+            // Access DB
+            // Get a list of all employees
+            // Display the list
+        }
+
+        if (answers.menuChoice == "add a department") {
+            console.log("Ask for the new department name to add. Add it to the DB.");
+            // Ask for new department name.
+            // Add it to the DB
+        }
+
+        if (answers.menuChoice == "add a role") {
+            console.log("Ask for the new role name to add. Add it to the DB.");
+            // Ask for new department name.
+            // Add it to the DB
+        }
+
+
+        if (answers.menuChoice == "add an employee") {
+            console.log("Ask for the new employee name to add. Add it to the DB.");
+            // Ask for new department name.
+            // Add it to the DB
+        }
+
+
+        if (answers.menuChoice == "update a new employee role") {
+            console.log("Ask for the new upated employee role. Add it to the DB.");
+            // Ask for new department name.
+            // Add it to the DB
+        }
+
+
+        if (answers.menuChoice == "quit") {
+            console.log("Ask to quit. Add it to the DB.");
+            // Ask for new department name.
+            // Add it to the DB
+        }
+        
+        // Finish adding response holders. XXXXXXXX
+
+
+
+
+
+
+
+
+
+        if (answers.menuChoice == "quit") {
+            console.log("Thank you for playing....");
+            exit = true; // leave the while loop
+        }
+
+    }
+}
+
+init();
+
+
+
+// {
+//     type: "list",
+//     name: "menuChoice",
+//     message: "Enter your github username:",
+//     when: (answers) => {
+//         if (answers.employeeTitle === "Engineer") {
+//             return true;
+//         }
+// }
 
 
